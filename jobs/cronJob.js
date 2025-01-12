a8985a239e33ae662fda4696aabda0c1aa774db5
Chaitanya Arora <Tanya242005@gmail.com>
2025-01-12 14:06:30 +0530

    Minor Fixes

--- jobs/cronJob.js
+++ jobs/cronJob.js
@@ -2,12 +2,12 @@ const cron = require('node-cron');
 const fetchCryptoData = require('../services/fetchCryptoData');
 
 // Schedule the job to run every 2 hours
-// cron.schedule('0 */2 * * *', () => {
-//     console.log('Running the crypto data fetch job...');
-//     fetchCryptoData();
-// });
-cron.schedule('* * * * *', () => { // Runs every 10 minutes
+cron.schedule('0 */2 * * *', () => {
     console.log('Running the crypto data fetch job...');
     fetchCryptoData();
 });
+// cron.schedule('* * * * *', () => { // Runs every 10 minutes
+//     console.log('Running the crypto data fetch job...');
+//     fetchCryptoData();
+// });
 // https://crontab.guru/
